217397bfd9ea2cf9a5a1f574c9f22178c5a89f53
Pavel Lapko <p.lapko@andersenlab.com>
2017-09-06 23:42:25 +0300

    Added some style adding and fixes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+//modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule,
         MdCheckboxModule,
@@ -15,12 +15,12 @@ import {MdButtonModule,
         MdDialogModule} from '@angular/material';
 import {CodemirrorModule} from 'ng2-codemirror';
 import {DndModule} from 'ng2-dnd';
-
+//components
 import {AppComponent } from './app.component';
 import {TypeConstructorComponent} from './components/type-constructor.component/type-constructor.component';
 import {AddTypeCardDialogComponent} from './components/add-card-dialog.component/add-card-dialog.component';
 import {TypeCardComponent} from './components/type-card.component/type-card.component';
-
+//pipes
 import {StringToBooleanPipe} from './pipes/stringBool';
 
 @NgModule({
